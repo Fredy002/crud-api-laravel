f99ad6d5a9b385c99a08f1ed9a5a48cd0d2152ad
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-03 12:22:28 -0500

    feat(migrations): create user table with name, gender, and status fields

---
+++ database/migrations/2024_10_03_171401_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->enum('sexo', ['Mujer', 'Varón']);
+            $table->date('fecha_nacimiento');
+            $table->enum('estado', ['Activo', 'Inactivo'])->default('Activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
