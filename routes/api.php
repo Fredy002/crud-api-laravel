3712e1bfe00c0b129ecd712ce72d30bdb3082b5a
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-03 12:22:47 -0500

    feat(routes): add basic CRUD routes for user management

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function () {
     return 'User List';
 });
+
+Route::get('/user', function () {
+    return 'Lista de usuarios';
+});
+
+Route::get('/user/{id}', function () {
+    return 'Usario obtenido';
+});
+
+Route::post('/user', function () {
+    return 'Creando usuario';
+});
+
+Route::put('/user/{id}', function () {
+    return 'Actualizando usuario';
+});
+
+Route::delete('/user/{id}', function () {
+    return 'Eliminando usuario';
+});
