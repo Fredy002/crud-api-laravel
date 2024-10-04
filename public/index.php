539ac1d8aab1f9b2b245d7c2d13d09ac363b8c56
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-03 20:48:10 -0500

    update deploy

--- public/index.php
+++ public/index.php
@@ -15,7 +15,12 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Handle the incoming request through Laravel's HTTP kernel
-$app->make(Illuminate\Contracts\Http\Kernel::class)->handle(
-    Request::capture()
-)->send();
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
